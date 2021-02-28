fcc3eb607d7b9ebc90267ee7ab5a6117a8c6786c
Carlos E. La Torre <amautacelt@gmail.com>
2021-02-28 00:56:57 -0500

    commit tree 46f07484c8bd692875db9ce99ae5e38f03e860b3

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,16 @@
 class Shoe
+
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    # BRANDS << brand
+    if !BRANDS.include?(brand)
+      BRANDS << brand
+    end
   end
 
   def cobble
@@ -12,3 +19,41 @@ class Shoe
   end
 
 end
+
+
+# class Shoe
+#   attr_accessor :color, :size, :material, :condition
+#   attr_reader :brand
+
+#   BRANDS = []
+
+#   def initialize(brand)
+#     @brand = brand
+#     if !(BRANDS.include?(@brand))
+#       BRANDS << @brand
+#     end
+#   end
+#
+#   def cobble
+#     self.condition = "new"
+#     puts "Your shoe is as good as new!"
+#   end
+
+# end
+
+##########
+
+# def initialize(brand)
+#   @brand = brand
+#   BRANDS << brand unless BRANDS.include? brand
+# end
+
+##########
+
+# def initialize(brand)
+#   @brand = brand
+#   # BRANDS.push(brand)
+#   if BRANDS.none? (brand)
+#     BRANDS.push(brand)
+#   end
+# end
